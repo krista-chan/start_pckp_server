c6ead602cc9db54c3ccf374f5baec538900b4aa7
Krista <65502847+krista-chan@users.noreply.github.com>
2021-07-05 18:27:21 +0100

    Fix y/n thingy

--- start_pckp_server.sh
+++ start_pckp_server.sh
@@ -4,14 +4,17 @@
 
 if ! command -v rustup &> /dev/null; then
   >&2 echo "Rustup could not be found on path."
-  echo "Would you like to install it? - y/n (default)"
+  echo "Would you like to install it? - y/n"
   
-  select yn in "y" "n"; do
+  while true; do
+    read -p "Do you wish to install rustup to be able to run this program?" yn
     case $yn in
-      y ) curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh; break;;
-      n ) exit;;
+      [Yy]* ) curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh; break;;
+      [Nn]* ) exit;;
+      * ) echo "Please answer yes or no";;
     esac
-   done
+  done
+
 fi
 
 git clone https://github.com/krista-chan/pckp-server
